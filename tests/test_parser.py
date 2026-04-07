83fd594ba4f23c91cd5c27e732d169a779125e6e
Srijan Tiwari <tiwarisrijan612@gmail.com>
2026-04-07 19:31:51 +0530

    Add unit tests for PDB parser validation (part 2/5)

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -31,3 +31,29 @@ def test_parse_atom_count():
     assert len(mol) == 5
 
 
+def test_parse_molecule_name_contains_chain():
+    mol = parse_pdb(MINI_PDB)[0]
+    assert mol.name.endswith("_A")
+
+
+def test_parse_source_fmt():
+    mol = parse_pdb(MINI_PDB)[0]
+    assert mol.source_fmt == "pdb"
+
+
+# ------------------------------------------------------------------ #
+#  Atom identity fields                                                #
+# ------------------------------------------------------------------ #
+
+def test_atom_names():
+    mol = parse_pdb(MINI_PDB)[0]
+    names = [a.atom_name for a in mol.atoms]
+    assert names == ["N", "CA", "C", "O", "N"]
+
+
+def test_atom_serials():
+    mol = parse_pdb(MINI_PDB)[0]
+    serials = [a.atom_serial for a in mol.atoms]
+    assert serials == [1, 2, 3, 4, 5]
+
+
