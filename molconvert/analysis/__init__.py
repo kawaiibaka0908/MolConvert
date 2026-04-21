81603fc300960183aa1de98444934cca4525cd59
Srijan Tiwari <tiwarisrijan612@gmail.com>
2026-04-21 18:55:50 +0530

    Initialize module namespace for analysis

---
+++ molconvert/analysis/__init__.py
@@ -0,0 +1,19 @@
+from .rmsd import (
+    rmsd,
+    kabsch_superpose,
+    kabsch_rmsd,
+    rmsd_molecules,
+    per_atom_deviation,
+    ic_summary,
+    ICSummary,
+)
+
+__all__ = [
+    "rmsd",
+    "kabsch_superpose",
+    "kabsch_rmsd",
+    "rmsd_molecules",
+    "per_atom_deviation",
+    "ic_summary",
+    "ICSummary",
+]
