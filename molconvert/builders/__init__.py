82be54bf82217263254c288cb36fe2dc6b7b3621
Srijan Tiwari <tiwarisrijan612@gmail.com>
2026-07-22 16:00:40 +0530

    feat(builders): export new builders in builders package init

--- molconvert/builders/__init__.py
+++ molconvert/builders/__init__.py
@@ -1,3 +1,17 @@
 from .reconstruct import reconstruct, to_pdb, save_pdb
+from .to_sdf import molecule_to_sdf, save_sdf, molecules_to_sdf, save_sdf_multi
+from .to_mol2 import molecule_to_mol2, save_mol2, molecules_to_mol2, save_mol2_multi
+from .to_xyz import molecule_to_xyz, save_xyz
+from .to_gro import molecule_to_gro, save_gro
+from .to_gaussian import molecule_to_gaussian, save_gaussian
+from .to_gamess import molecule_to_gamess, save_gamess
 
-__all__ = ["reconstruct", "to_pdb", "save_pdb"]
+__all__ = [
+    "reconstruct", "to_pdb", "save_pdb",
+    "molecule_to_sdf", "save_sdf", "molecules_to_sdf", "save_sdf_multi",
+    "molecule_to_mol2", "save_mol2", "molecules_to_mol2", "save_mol2_multi",
+    "molecule_to_xyz", "save_xyz",
+    "molecule_to_gro", "save_gro",
+    "molecule_to_gaussian", "save_gaussian",
+    "molecule_to_gamess", "save_gamess",
+]
