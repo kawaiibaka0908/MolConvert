1775b2906c5740ec8fca9b97ca4896f76fcfbb96
Srijan Tiwari <tiwarisrijan612@gmail.com>
2026-07-27 17:40:05 +0530

    chore(config): update gitignore and bump package version to 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="molconvert",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     python_requires=">=3.9",
     install_requires=[
